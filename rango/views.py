aa2f5658f137a926cdf7da6ea84974ad15cb8d67
Andrew Laing <2775878l@student.gla.ac.uk>
2024-02-07 02:50:07 +0000

    Database implemented

--- rango/views.py
+++ rango/views.py
@@ -3,4 +3,10 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Rango says there partner!")
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
+
+
+def about(request):
+    context_dict = {'boldmessage': 'This tutorial has been put together by Oliver Swannick'}
+    return render(request, 'rango/about.html', context=context_dict)
